1af8815983b1d96d419019a4d4586aa8ec3c85a6
Green, Tal (tg4920) <tg4920@intl.att.com>
2019-11-03 17:02:08 +0200

    sefsers

--- chatbox.js
+++ chatbox.js
@@ -139,17 +139,8 @@ var Fake = [
   'Nice to meet you',
   'How are you?',
   'Not too bad, thanks',
-  'What do you do?',
-  'That\'s awesome',
-  'Codepen is a nice place to stay',
-  'I think you\'re a nice person',
-  'Why do you think that?',
-  'Can you explain?',
-  'Anyway I\'ve gotta go now',
-  'It was a pleasure chat with you',
-  'Time to make a new codepen',
   'Bye',
-  ':)'
+  '<div class="coupon"><div class="coupon__tag">✶ Coupon ✶</div><div class="coupon__body"><div class="coupon__title">Coca Cola</div><div class="coupon__value"><strong>-5$</strong></div></div></div>'
 ]
 
 function fakeMessage() {
