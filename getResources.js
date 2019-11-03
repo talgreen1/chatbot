e028574654def9d0ae96190146ef28139d7e1de3
Green, Tal (tg4920) <tg4920@intl.att.com>
2019-11-03 13:50:23 +0200

    kj

--- getResources.js
+++ getResources.js
@@ -1,7 +1,7 @@
 function getResources(userSegment) {
   var responseFromServer = {
-    "idleUrl": "https://cdn.glitch.com/70de237d-3155-4aee-89f1-4dffb32733f6%2F2_crop.gif?v=1572771897500",
-    "typingUrl": "https://cdn.glitch.com/70de237d-3155-4aee-89f1-4dffb32733f6%2F2_crop.gif?v=1572771897500",
+    "idleUrl": "assets/idle.gif",
+    "typingUrl": "assets/typing.gif",
     "couponText": "Get limited cup 50% off!",
     "couponUrl": "https://www.amazon.com/adidas-Bottles-Training-Performance-Running/dp/B078TBLJ6P"
   };
